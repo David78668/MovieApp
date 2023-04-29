a894a48c552f008b36b620cb485c724edcbd77bd
horak.david <horak.david@ssakhk.cz>
2023-04-29 12:33:49 +0200

    Initial commit

---
+++ MovieApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApp.Models;
+using MovieApp.Services;
+using System.Diagnostics;
+
+namespace MovieApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ImdbApiService _api;
+
+        public HomeController(ILogger<HomeController> logger, ImdbApiService api)
+        {
+            _logger = logger;
+            _api = api;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Search(string input)
+        {
+            List<SearchResponse> Movies = (List<SearchResponse>)_api.Search(input).Result;
+            return View(Movies);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ MovieApp/Services/ImdbApiService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieApp.Models;
+
+namespace MovieApp.Services
+{
+    public class ImdbApiService
+    {
+        private readonly HttpClient _client;
+        private readonly ILogger<ImdbApiService> _logger;
+
+        public ImdbApiService(ILogger<ImdbApiService> logger)
+        {
+            _client = new();
+            _logger = logger;
+        }
+
+        private async Task<T?> Download<T>(string url)
+        {
+            try
+            {
+                return await _client.GetFromJsonAsync<T>(url);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{url} - {ex.Message}");
+            }
+            return default(T?);
+        }
+
+
+        public async Task<IEnumerable<SearchResponse>> Search(string input)
+        {
+            return await Download<IEnumerable<SearchResponse>>($"https://imdb-api.com/API/Search/k_4556bqwf/{input}");
+        }
+    }
+}
